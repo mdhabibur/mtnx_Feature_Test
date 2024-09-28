41c99b95136e48f0e8d4f41b2ec062f41718937a
mdhabibur <mdhrahman532@gmail.com>
2024-09-28 20:02:18 +0600

    MotionX frontend project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-green-500 font-bold text-center text-3xl py-5'>
+      Motion X Frontend
+    </div>
+  )
+}
+
+export default App
