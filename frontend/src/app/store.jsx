8bbc9686dcbf9213b4a26f69d0ca0e7f72c69bd4
mdhabibur <mdhrahman532@gmail.com>
2024-10-27 03:55:23 +0600

    redux-persist implemented for persistent redux auth state across app

--- frontend/src/app/store.jsx
+++ frontend/src/app/store.jsx
@@ -1,11 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from '../redux/auth/authSlice'
+import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from 'redux-persist';
 
+const persistConfig = {
+    key: 'root',
+    storage,
+  };
+
+//persisted reducer
+const persistedReducer = persistReducer(persistConfig, authReducer)
 
 const store = configureStore({
     reducer: {
-        auth: authReducer
-    }
+        auth: persistedReducer, //Persisted reducer
+
+    },
+    middleware: (getDefaultMiddleware) => 
+        getDefaultMiddleware({
+            serializableCheck: false
+        })
 })
+//create a persistor
+export const persistor = persistStore(store);
 
 export default store
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {Provider} from 'react-redux'
-import store from './app/store.jsx'
+import store, { persistor } from './app/store.jsx'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
   <Provider store = {store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
     
   
